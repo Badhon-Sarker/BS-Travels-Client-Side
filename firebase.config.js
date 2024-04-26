2a17bcbe2bd6cec21e962b181b0783a5180fa7f3
Badhon-Sarker <badhonsarker38@gmail.com>
2024-04-27 04:48:10 +0600

    variable added

--- firebase.config.js
+++ firebase.config.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBvCJ9KODJ4aMFXMM09pFRhCBd8lgNSJB0",
-  authDomain: "assignment-10-535b2.firebaseapp.com",
-  projectId: "assignment-10-535b2",
-  storageBucket: "assignment-10-535b2.appspot.com",
-  messagingSenderId: "20055450256",
-  appId: "1:20055450256:web:22d3014afdee1e0aee2e15"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
